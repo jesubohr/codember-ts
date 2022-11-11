9ab68d68f5d524ffa43cd21ab915296f8c508405
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-11-11 00:47:51 -0500

    Init Project

---
+++ README.md
@@ -0,0 +1,8 @@
+# ¡CODEMBER 2022!
+Soluciones de los retos propuestos cada semana en [Codember](https://codember.dev) desarrollados en [TypeScript](https://www.typescriptlang.org/).
+
+## Retos semanales
+
+
+## Ejecutar los retos
+Cada reto se ejecuta con el comando `npm start` dentro de su respectiva carpeta.
---
+++ index.js
@@ -0,0 +1,13 @@
+import { spawn } from "node:child_process";
+
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+  console.log('Usage: npm start <number>');
+  process.exit(1);
+}
+
+console.log('Executing Challenge', args[0]);
+spawn('node', [`./"RETO ${args[0]}"`], {
+  stdio: "inherit",
+  shell: true,
+});
