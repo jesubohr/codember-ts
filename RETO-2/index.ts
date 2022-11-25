4de01da5989f9e6693496ee1a940176254bc61db
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-11-25 18:23:31 -0500

    [done] Reto-2

--- RETO-2/index.ts
+++ RETO-2/index.ts
@@ -1,1 +1,13 @@
-console.log("Reto 2")
+import { readFile } from '../utils/readFile.js'
+
+const ASCIIRegex = new RegExp(/1\d{2}|[2-9]\d{1}/, 'g')
+function decryptString (encryptedString: string) {
+  const encryptedWords = encryptedString.split(' ')
+  return encryptedWords.map(word => {
+    const asciiCodes = word.match(ASCIIRegex) as string[]
+    return String.fromCharCode(...asciiCodes.map(code => parseInt(code)))
+  }).join(' ')
+}
+
+const encryptedString = readFile('RETO-2/encrypted.txt')
+console.log(decryptString(encryptedString))
